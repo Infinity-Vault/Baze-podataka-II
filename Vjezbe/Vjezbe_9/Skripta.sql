4b8428f356dd4d3bd482440e5870c7b1938e76b6
Haris Kordić <kordicharis18@gmail.com>
2022-05-16 15:22:13 +0200

    add skripta za vjezbe_9

---
+++ Vjezbe/Vjezbe_9/Skripta.sql
@@ -0,0 +1,4 @@
+INSERT INTO Sales.CreditCard (CardType, CardNumber, ExpMonth, ExpYear) VALUES ('Visa', '11111111111111', '7', '2024')
+INSERT INTO Sales.PersonCreditCard (BusinessEntityID, CreditCardID, ModifiedDate) VALUES (745, 19238, '2022-05-10 00:00:00.000')
+INSERT INTO Sales.CreditCard (CardType, CardNumber, ExpMonth, ExpYear) VALUES ('Maestro', '22222222222222', '11', '2024')
+INSERT INTO Sales.PersonCreditCard (BusinessEntityID, CreditCardID, ModifiedDate) VALUES (2546, 19239, '2022-05-10 00:00:00.000')
