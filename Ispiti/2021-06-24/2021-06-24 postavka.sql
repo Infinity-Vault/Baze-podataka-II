7d74d27ee8d7e48c1e50f50472f2a57a6a206a55
Haris Kordić <kordicharis18@gmail.com>
2022-07-07 20:16:37 +0200

    Update 2021-06-24 postavka.sql

--- Ispiti/2021-06-24/2021-06-24 postavka.sql
+++ Ispiti/2021-06-24/2021-06-24 postavka.sql
@@ -1,282 +1,226 @@
-﻿/*
-Napomena:
-A.
-Prilikom  bodovanja rješenja prioritet ima rezultat 
-koji upit treba da vrati (broj zapisa, vrijednosti agregatnih funkcija...).
-U slučaju da rezultat upita nije tačan, a rezultat tog upita se koristi
-u narednim zadacima, tada se rješenja narednih zadataka, 
-bez obzira na tačnost koda, ne boduju punim brojem bodova, 
-jer ni ta rješenja ne mogu vratiti tačan rezultat 
-(broj zapisa, vrijednosti agregatnih funkcija...).
-B.
-Tokom pisanja koda obratiti pažnju na tekst zadatka 
-i ono što se traži zadatkom. Prilikom pregleda rada pokreće se 
-kod koji se nalazi u sql skripti i 
-sve ono što nije urađeno prema zahtjevima zadatka 
-ili je pogrešno urađeno predstavlja grešku. 
-*/
-
-------------------------------------------------
-/*
-BODOVANJE
-	Maksimalni broj bodova:		80
-	Prag prolaznosti:			44
-RASPON OCJENA
-	bodovi			ocjena
-	0	-	43		5
-	44	-	58		6
-	59	-	73		7
-	74	-	80		8
-*/
-------------------------------------------------
-
-
-
-------------------------------------------------
---1. 
-/*
-Kreirati bazu podataka pod vlastitim brojem indeksa
-i aktivirati je.
-*/
-
----------------------------------------------------------------------------
---Prilikom kreiranja tabela voditi računa o njihovom međusobnom odnosu.
----------------------------------------------------------------------------
-/*
-a) 
-Kreirati tabelu prodavac koja će imati sljedeću strukturu:
-	- prodavac_id, cjelobrojni tip, primarni ključ
-	- naziv_posla, 50 unicode karaktera
-	- dtm_rodj, datumski tip
-	- bracni_status, 1 karakter
-	- prod_kvota, novčani tip
-	- bonus, novčani tip
-*/
-
-/*
-b) 
-Kreirati tabelu prodavnica koja će imati sljedeću strukturu:
-	- prodavnica_id, cjelobrojni tip, primarni ključ
-	- naziv_prodavnice, 50 unicode karaktera
-	- prodavac_id, cjelobrojni tip
-*/
-
-/*
-c) 
-Kreirati tabelu kupac_detalji koja će imati sljedeću strukturu:
-	- detalj_id, cjelobrojni tip, primarni ključ, automatsko punjenje sa početnom vrijednošću 1 i inkrementom 1
-	- kupac_id, cjelobrojni tip, primarni ključ
-	- prodavnica_id, cjelobrojni tip
-	- br_rac, 10 karaktera
-	- dtm_narudz, datumski tip
-	- kolicina, skraćeni cjelobrojni tip
-	- cijena, novčani tip
-	- popust, realni tip
-*/
---10 bodova
-
-
-
-
-
---2.
-/*
-a)
-Koristeći tabele HumanResources.Employee i Sales.SalesPerson
-baze AdventureWorks2017 zvršiti insert podataka u 
-tabelu prodavac prema sljedećem pravilu:
-	- BusinessEntityID -> prodavac_id
-	- JobTitle -> naziv_posla
-	- BirthDate -> dtm_rodj
-	- MaritalStatus -> bracni_status
-	- SalesQuota -> prod_kvota
-	- Bonus -> nžbonus
-*/
-
-/*
-b)
-Koristeći tabelu Sales.Store baze AdventureWorks2017 
-izvršiti insert podataka u tabelu prodavnica 
-prema sljedećem pravilu:
-	- BusinessEntityID -> prodavnica_id
-	- Name -> naziv_prodavnice
-	- SalesPersonID -> prodavac_id
-*/
-
-/*
-b)
-Koristeći tabele Sales.Customer, Sales.SalesOrderHeader i SalesOrderDetail
-baze AdventureWorks2017 izvršiti insert podataka u tabelu kupac_detalji
-prema sljedećem pravilu:
-	- CustomerID -> kupac_id
-	- StoreID -> prodavnica_id
-	- AccountNumber -> br_rac
-	- OrderDate -> dtm_narudz
-	- OrderQty -> kolicina
-	- UnitPrice -> cijena
-	- UnitPriceDiscount -> popust
-Uslov je da se ne dohvataju zapisi u kojima su 
-StoreID i PersonID NULL vrijednost
-*/
---10 bodova
-
-
-
-
-
-
---3.
-/*
-a)
-U tabeli prodavac dodati izračunatu kolonu god_rodj
-u koju će se smještati godina rođenja prodavca.
-b)
-U tabeli kupac_detalji promijeniti tip podatka
-kolone cijena iz novčanog u decimalni tip oblika (8,2)
-c)
-U tabeli kupac_detalji dodati standardnu kolonu
-lozinka tipa 20 unicode karaktera.
-d) 
-Kolonu lozinka popuniti tako da bude spojeno 
-10 slučajno generisanih znakova i 
-numerički dio (bez vodećih nula) iz kolone br_rac
-*/
---10 bodova
-
-
-
-
-
-
---4.
-/*
-Koristeći tabele prodavnica i kupac_detalji
-dati pregled sumiranih količina po 
-nazivu prodavnice i godini naručivanja.
-Sortirati po nazivu prodavnice.
-*/
---6 bodova
-
-
-
-
-
-
---5.
-/*
-Kreirati pogled v_prodavac_cijena sljedeće strukture:
-	- prodavac_id
-	- bracni_status
-	- sum_cijena
-Uslov je da se u pogled dohvate samo oni zapisi u 
-kojima je sumirana vrijednost veća od 1000000.
-*/
---8 bodova
-
-
-
-
-
-
---6.
-/*
-Koristeći pogled v_prodavac_cijena
-kreirati proceduru p_prodavac_cijena sa parametrom
-bracni_status čija je zadata (default) vrijednost M.
-Uslov je da se procedurom dohvataju zapisi u kojima je 
-vrijednost u koloni sum_cijena veća od srednje vrijednosti kolone sum_cijena.
-Obavezno napisati kod za pokretanje procedure.
-*/
---8 bodova
-
-
-
-
-
-
---7.
-/*
-Iz tabele kupac_detalji prikazati zapise u kojima je 
-vrijednost u koloni cijena jednaka 
-minimalnoj, odnosno, maksimalnoj vrijednosti u ovoj koloni.
-Upit treba da vraća kolone kupac_id, prodavnica_id i cijena.
-Sortirati u rastućem redoslijedu prema koloni cijena.
-*/
---8 bodova
-
-
-
-
-
-
---8.
-/*
-a)
-U tabeli kupac_detalji kreirati kolonu
-cijena_sa_popustom tipa decimal (8,2).
-b) 
-Koristeći tabelu kupac_detalji
-kreirati proceduru p_popust sa parametrom 
-godina koji će odgovarati godini iz datum naručivanja.
-Procedura će vršiti update kolone cijena_sa_popustom
-ako je vrijednost parametra veća od 2013, 
-inače se daje poruka 'transakcija nije izvršena'.
-Testirati funkcionisanje procedure za vrijednost 
-parametra godina 2014.
-Obavezno napisati kod za provjeru sadržaja tabele 
-nakon što se pokrene procedura.
-*/
---8 bodova
-
-
-
-
-
-
---9.
-/*
-a)
-U tabeli prodavac kreirati kolonu min_kvota tipa decimal (8,2).
-i na njoj postaviti ograničenje da se
-ne može unijeti negativna vrijednost.
-b)
-Kreirati skalarnu funkciju f_kvota sa parametrom prod_kvota.
-Funkcija će vraćati rezultat tipa decimal (8,2)
-koji će se računati po pravilu:
-	10% od prod_kvota
-c) 
-Koristeći funkciju f_kvota izvršiti update
-kolone min_kvota u tabeli prodavac
-*/
---8 bodova
-
-
-
-
-
-
---10.
-/*
-a)
-Kreirati tabelu prodavac_log strukture:
-	- log_id, primarni ključ, automatsko punjenje sa početnom vrijednošću 1 i inkrementom 1 
-	- prodavac_id int
-	- min_kvota decimal (8,2)
-	- dogadjaj varchar (3)
-	- mod_date datetime
-b)
-Nad tabelom prodavac kreirati okidač t_ins_prod
-kojim će se prilikom inserta podataka u 
-tabelu prodavac izvršiti insert podataka u 
-tabelu prodavac_log sa naznakom aktivnosti 
-(insert, update ili delete).
-c)
-U tabelu autori insertovati zapis
-291, Sales Manager, 1985-09-30, M, 250000.00, 985.00, -20000.00
-Ako je potrebno izvršiti podešavanja 
-koja će omogućiti insrt zapisa. 
-d)
-Obavezno napisati kod za pregled sadržaja 
-tabela prodavac i prodavac_log.
-*/
---4 boda
+--Napomena:
+--A.
+--Prilikom  bodovanja rje�enja prioritet ima rezultat 
+--koji upit treba da vrati (broj zapisa, vrijednosti agregatnih funkcija...).
+--U slu?aju da rezultat upita nije ta?an, a pogled, tabela... 
+--koji su rezultat tog upita se koriste u narednim zadacima, 
+--tada se rje�enja narednih zadataka, bez obzira na ta?nost koda, 
+--ne boduju punim brojem bodova, jer ni ta rje�enja ne mogu vratiti ta?an rezultat 
+--(broj zapisa, vrijednosti agregatnih funkcija...).
+--B.
+--Tokom pisanja koda obratiti pa�nju na tekst zadatka 
+--i ono �to se tra�i zadatkom. Prilikom pregleda rada pokre?e se 
+--kod koji se nalazi u sql skripti i sve ono �to nije ura?eno prema zahtjevima zadatka 
+--ili je pogre�no ura?eno predstavlja gre�ku. 
+--Shodno navedenom, na uvidu se ne prihvata prigovor 
+--da je neki dio koda posljedica previda ("nisam vidio", "slu?ajno sam to napisao"...) 
+--
+--
+--------------------------------------------------
+--
+----1.
+--
+--a) Kreirati bazu pod vlastitim brojem indeksa.
+--
+--
+-----------------------------------------------------------------------------
+----Prilikom kreiranja tabela voditi ra?una o njihovom me?usobnom odnosu.
+-----------------------------------------------------------------------------
+--
+--b) Kreirati tabelu kreditna sljede?e strukture:
+--	- kreditnaID - cjelobrojni tip, primarni klju?
+--	- tip_kreditne - 50 unicode karaktera
+--	- br_kreditne - 25 unicode karatera, obavezan unos
+--	- dtm_evid - datumska varijabla za unos datuma
+--
+--c) Kreirati tabelu osoba sljede?e strukture:
+--	- osobaID - cjelobrojni tip, primarni klju?
+--	- kreditnaID - cjelobrojni tip, obavezan unos
+--	- mail_lozinka - 50 unicode karaktera
+--	- lozinka - 10 unicode karaktera 
+--	- br_tel - 25 unicode karaktera
+--Na koloni mail_lozinka postaviti ograni?enje 
+--kojim se omogu?uje unos podatka koji ima 
+--maksimalno 20 karaktera.
+--
+--d) Kreirati tabelu narudzba sljede?e strukture:
+--	- narudzbaID - cjelobrojni tip, primarni klju?
+--	- kreditnaID - cjelobrojni tip
+--	- br_narudzbe - 25 unicode karaktera
+--	- br_racuna - 15 unicode karaktera
+--	- prodavnicaID - cjelobrojni tip
+--
+----2. 
+--
+--a) 
+--Iz tabele Sales.CreditCard baze AdventureWorks2017 
+--importovati podatke u tabelu kreditna na sljede?i na?in:
+--	- CreditCardID -> kreditnaID
+--	- CardNUmber -> br_kreditne
+--	- ModifiedDate -> dtm_evid
+--	- kreditnaID - cjelobrojni tip, primarni klju?
+--	- tip_kreditne - 50 unicode karaktera
+--	- br_kreditne - 25 unicode karatera, obavezan unos
+--	- dtm_evid - datumska varijabla za unos datuma
+--
+--
+--b) 
+--Iz tabela Person.Person, Person.Password, 
+--Sales.PersonCreditCard i Person.PersonPhone 
+--baze AdventureWorks2017 
+--importovati podatke u tabelu osoba na sljede?i na?in:
+--	- BussinesEntityID -> osobaID
+--	- CreditCardID -> kreditnaID
+--	- PasswordHash -> mail_lozinka
+--	- PasswordSalt -> lozinka
+--	- PhoneNumber -> br_tel
+--Prilikom importa voditi ra?una o ograni?enju
+--na koloni mail_lozinka.
+--- osobaID - cjelobrojni tip, primarni klju?
+--	- kreditnaID - cjelobrojni tip, obavezan unos
+--	- mail_lozinka - 50 unicode karaktera
+--	- lozinka - 10 unicode karaktera 
+--	- br_tel - 25 unicode karaktera
+--
+--
+--c) 
+--Iz tabela Sales.Customer i Sales.SalesOrderHeader baze AdventureWorks2017
+--importovati podatke u tabelu narudzba na sljede?i na?in:
+--	- SalesOrderID -> narudzbaID
+--	- CreditCardID -> kreditnaID
+--	- PurchaseOrderNumber -> br_narudzbe
+--	- AccountNumber -> br_racuna
+--	- StoreID -> prodavnicaID
+--	- narudzbaID - cjelobrojni tip, primarni klju?
+--	- kreditnaID - cjelobrojni tip
+--	- br_narudzbe - 25 unicode karaktera
+--	- br_racuna - 15 unicode karaktera
+--	- prodavnicaID - cjelobrojni tip
+--
+--3---
+--a)
+--U tabeli kreditna dodati novu izra?unatu kolonu
+--god_evid u koju ?e se smje�tati godina iz kolone dtm_evid
+--b)
+--U tabeli kreditna izvr�iti update kolone tip_kreditne
+--tako �to ?e se Vista zamijeniti sa Visa
+--c)
+--U tabeli osoba izvr�iti update kolone
+--mail_lozinka u svim zapisima u kojima 
+--se podatak u mail_lozinka zavr�ava bilo kojom cifrom.
+--Update izvr�iti tako da se umjesto cifre postavi znak @.
+--
+--
+--
+--
+----4.
+--
+--Koriste?i tabele kreditna i osoba kreirati 
+--pogled view_kred_mail koji ?e se sastojati od kolona: 
+--	- br_kreditne, 
+--	- mail_lozinka, 
+--	- br_tel i 
+--	- br_cif_br_tel, 
+--pri ?emu ?e se kolone puniti na sljede?i na?in:
+--	- br_kreditne - odbaciti prve 4 cifre 
+-- 	- mail_lozinka - preuzeti sve znakove od znaka na 10. mjestu (uklju?iti i njega)
+--	- br_tel - prenijeti cijelu kolonu
+--	- br_cif_br_tel - broj znakova (cifara) u koloni br_tel
+--
+--
+----5.
+--/*
+--a)
+--Iz pogleda view_kred_mail kreirati tabelu kred_mail
+--b)
+--Nad tabelom kred_mail kreirati proceduru p_del_kred_mail 
+--tako da se obri�u svi zapisi u kojima se 
+--broj kreditne kartice zavr�ava neparnom cifrom.
+--Nakon kreiranja pokrenuti proceduru.
+--c) 
+--U tabeli kred_mail kreirati izra?unatu kolonu indikator
+--koja ?e puniti prema pravilu: 
+--	- br_cif_br_tel = 12, indikator = 0
+--	- br_cif_br_tel = 19, indikator = 1
+--
+----6.
+--
+--a)
+--Kopirati tabelu kreditna u kreditna1, 
+--b)
+--U tabeli kreditna1 dodati novu kolonu dtm_aktivni 
+--?ija je default vrijednost aktivni datum sa vremenom. 
+--Kolona je sa obaveznim unosom.
+--c) 
+--U tabeli kreditna1 dodati novu kolonu br_mjeseci 
+--koja ?e broj mjeseci izme?u aktivnog datuma i datuma evidencije.
+--d) 
+--Prebrojati broj zapisa u tabeli kreditna1 
+--kod kojih se datum evidencije nalazi u intevalu 
+--do najvi�e 84 mjeseca u odnosu na aktivni datum.
+--
+--
+----7.
+--
+--Iz tabele narudzba jednim upitom:
+--	-	prebrojati broj zapisa u kojima je u koloni
+--		br_narudzbe NULL vrijednost
+--	-	prebrojati broj zapisa u kojima je u koloni
+--		prodavnicaID NULL vrijednost
+--Upit treba da vrati rezultat u obliku:
+--	broj NULL u br_narudzbe	je	(navesti broj zapisa)
+--	broj NULL u prodavnicaID je	(navesti broj zapisa)
+--*/
+--
+----8.
+--/*
+--a)
+--Koriste?i tabelu narudzba kreirati 
+--pogled v_duz_br_nar strukture:
+--	- broj karaktera u koloni br_narudzbe
+--	- prebrojani broj zapisa prema broju karaktera
+--b)
+--Koriste?i pogled v_duz_br_nar dati pregled
+--zapisa u kojima se prebrojani broj nalazi u rasponu 
+--do maksimalno 1800 u odnosu na minimalnu vrijednost u koloni prebrojano, 
+--uz uslov da se ne prikazuje minimalna vrijednost
+--
+----9.
+--
+--Koriste?i tabelu narudzba 
+--kreirati funkciju f_pocetak koja vra?a podatke
+--u formi tabele sa parametrima:
+--	- poc_br_rac, 7 karaktera
+--	- kreditnaID, cjelobrojni tip
+--Parametar poc_br_rac se referira na 
+--prvih 7 karaktera kolone br_racuna,
+--pri ?emu je njegova zadana (default) vrijednost 10-4020.
+--kreditnaID se referira na kolonu kreditnaID.
+--Funkcija vra?a kolone kreditnaID, br_narudzbe i br_racuna.
+--uz uslov da se vra?aju samo zapisi kod kojih je 
+--kreditnaID ve?i od 10000.
+--Provjeriti funkcioniranje funkcije za kreditnaID = 1200.
+--Rezultat sortirati prema kreditnaID.
+--
+----10.
+--
+--a)
+--Kreirati tabelu kreditna1_log strukture:
+--	- log_id, primarni klju?, automatsko punjenje sa po?etnom vrijedno�?u 1 i inkrementom 1 
+--	- kreditnaID int
+--	- br_kreditne nvarchar (25)
+--	- br_mjeseci int
+--	- dogadjaj varchar (3)
+--	- mod_date datetime
+--b)
+--Nad tabelom kreditna1 kreirati okida? t_upd_kred
+--kojim ?e se prilikom update podataka u 
+--tabelu prodavac izvr�iti insert podataka u 
+--tabelu prodavac_log.
+--c)
+--U tabelu kreditna updatovati zapise tako da se
+--u svim zapisima u koloni br_kreditne 
+--po?etne niz cifara 1111 promijeni u 2222.
+--d)
+--Obavezno napisati kod za pregled sadr�aja 
+--tabela kreditna1 i kreditna1_log.
