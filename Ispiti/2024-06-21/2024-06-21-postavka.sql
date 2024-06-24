d5cadf4e71075fce509ff7722aa6a3bd1838d2ab
MujoBabajic <77216264+MujoBabajic@users.noreply.github.com>
2024-06-25 00:29:56 +0200

    Add files via upload

---
+++ Ispiti/2024-06-21/2024-06-21-postavka.sql
@@ -0,0 +1,90 @@
+﻿--1. Kroz SQL kod kreirati bazu podataka sa imenom vaseg broja indeksa
+
+--2. U kreiranoj bazi podataka kreirati tabele sa sljedecom strukturom:
+--a)	Uposlenici
+--•	UposlenikID, cjelobrojni tip i primarni kljuc, autoinkrement,
+--•	Ime 10 UNICODE karaktera obavezan unos,
+--•	Prezime 20 UNICODE karaktera obavezan unos
+--•	DatumZaposlenja polje za unos datuma i vremena obavezan unos
+--•	UkupanBrojTeritorija, cjelobrojni tip
+
+--b)	Narudzbe
+--•	NarudzbaID, cjelobrojni tip i primarni kljuc, autoinkrement
+--•	UposlenikID, cjelobrojni tip, strani kljuc,
+--•	DatumNarudzbe, polje za unos datuma i vremena,
+--•	ImeKompanijeKupca, 40 UNICODE karaktera,
+--•	AdresaKupca, 60 UNICODE karaktera
+
+--c) Proizvodi
+--•	ProizvodID, cjelobrojni tip i primarni ključ, auloinkrement
+--•	NazivProizvoda, 40 UNICODE karaktera (obavezan unos)
+--•	NazivKompanijeDobavljaca, 40 UNICODE karaktcra
+--•	NazivKategorije, 15 UNICODE Icaraktera
+
+--d) StavkeNarudzbe
+--•	NarudzbalD, cjelobrojni tip strani i primami ključ
+--•	ProizvodlD, cjelobrojni tip strani i primami ključ
+--•	Cijena, novčani tip (obavezan unos)
+--•	Kolicina, kratki cjelobrojni tip (obavezan unos)
+--•	Popust, real tip podatka (obavezan unos)
+
+--4 boda
+
+--3. Iz baze podataka Northwind u svoju bazu podataka prebaciti sljedeće podatke:
+--a) U tabelu Uposlenici dodati sve uposlenike
+--•	EmployeelD -> UposlenikID
+--•	FirstName -> Ime
+--•	LastNamc -> Prezime
+--•	BirthDate -> DatumRodjenja
+--•	lzračunata vrijednost za svakog uposlenika na osnovu EmployeeTerritories-:----UkupanBrojTeritorija
+
+--b) U tabelu Narudzbe dodati sve narudzbe
+--•	OrderlD -> NarudzbalD
+--•	EmployeelD -> UposlenikID
+--•	OrderDate -> DatumNarudzbe
+--•	CompanyName -> ImeKompanijeKupca
+--•	Address -> AdresaKupca
+
+--c) U tabelu Proizvodi dodati sve proizvode
+--•	ProductID -> ProizvodlD
+--•	ProductName -> NazivProizvoda
+--•	CompanyName -> NazivKompanijeDobavljaca
+--•	CategoryName -> NazivKategorije
+
+--d) U tabelu StavkeNarudzbe dodati sve stavke narudzbe
+--•	OrderlD -> NarudzbalD
+--•	ProductID -> ProizvodlD
+--•	UnitPrice -> Cijena
+--•	Quantity -> Kolicina
+--•	Discount -> Popust
+
+--5 bodova
+
+--4. 
+--a) (4 boda) U tabelu StavkeNaudzbe dodati 2 nove izračunate kolone: vrijednostNarudzbeSaPopustom i vrijednostNarudzbeBezPopusta. Izzačunate kolonc već čuvaju podatke na osnovu podataka iz kolona! 
+
+--b) (5 bodom) Kreirati pogled v_select_orders kojim ćc se prikazati ukupna zarada po uposlenicima od narudzbi kreiranih u zadnjem kvartalu 1996. godine. Pogledom je potrebno prikazati spojeno ime i prezime uposlenika, ukupna zarada sa popustom zaokrzena na dvije decimale i ukupna zarada bez popusta. Za prikaz ukupnih zarada koristiti OBAVEZNO koristiti izračunate kolone iz zadatka 4a. (Novokreirana baza)
+
+--c) (5 boda) Kreirati funkciju f_starijiUposleici koja će vraćati podatke u formi tabele na osnovu proslijedjenog parametra godineStarosti, cjelobrojni tip. Funkcija će vraćati one zapise u kojima su godine starosti kod uposlenika veće od unesene vrijednosti parametra. Potrebno je da se prilikom kreiranja funkcije u rezultatu nalaze sve kolone tabele uposlenici, zajedno sa izračunatim godinama starosti. Provjeriti ispravnost funkcije unošenjem kontrolnih vrijednosti. (Novokreirana baza) 
+
+--d) (7 bodova) Pronaći najprodavaniji proizvod u 2011 godini. Ulogu najprodavanijeg nosi onaj kojeg je najveći broj komada prodat. (AdventureWorks2017)
+
+--e) (6 bodova) Prikazati ukupan broj proizvoda prema specijalnim ponudama. Potrebno je prebrojati samo one proizvode koji pripadaju kategoriji odjeće. (AdventureWorks2017) 
+
+--f) (8 bodova) Prikazati najskuplji proizvod (List Price) u svakoj kategoriji. (AdventureWorks2017) 
+
+--g) (8 bodova) Prikazati proizvode čija je maloprodajna cijena (List Price) manja od prosječne maloprodajne cijene kategorije proizvoda kojoj pripada. (AdventureWorks2017) 
+
+--43 boda
+
+--5. 
+--a) (12 bodova) Pronaći najprodavanije proizvode, koji nisu na lisli top 10 najprodavanijih proizvoda u zadnjih 11 godina. (AdventureWorks2017) 
+
+--b) (16 bodova) Prikazati ime i prezime kupca, id narudzbe, te ukupnu vrijednost narudzbe sa popustom (zaokruzenu na dvije decimale), uz uslov da su na nivou pojedine narudžbe naručeni proizvodi iz svih kategorija. (AdventureWorks2017) 
+
+--28 bodova 
+
+--6. Dokument teorijski_ispit 21 JUN24, preimcnovati vašim brojem indeksa, te u tom dokumentu izraditi pitanja. 
+
+--20 bodova 
+
